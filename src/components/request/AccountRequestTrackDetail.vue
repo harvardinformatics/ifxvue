0dafff8797aa7687ee06d3af94067edcc84fbfb4
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2020-08-21 10:28:51 -0400

    Initial move of request, account request items into p3

---
+++ src/components/request/AccountRequestTrackDetail.vue
@@ -0,0 +1,65 @@
+<script>
+// Display data common to all account requests
+import DisplayAddressList from './DisplayAddressList'
+import DisplayContactList from './DisplayContactList'
+import DisplayHarvardKey from './DisplayHarvardKey'
+
+export default {
+  name: 'AccountRequestTrackDetail',
+  props: {
+    accountRequestData: Object,
+    track: String,
+    trackTitle: String
+  },
+  components: {
+    DisplayAddressList,
+    DisplayContactList,
+    DisplayHarvardKey
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<template>
+  <v-container  v-if="accountRequestData" >
+    <v-layout column>
+      <v-flex>
+        <span class="title">{{trackTitle}}</span>
+      </v-flex>
+      <v-flex v-for="field in accountRequestData.tracks[track].fields.order" :key="field">
+        <v-layout row wrap v-if="accountRequestData.tracks[track].fields[field]" justify-start>
+          <v-flex class="field-label" xs12 md3 v-if="accountRequestData.tracks[track].fields[field].display_name">
+            {{accountRequestData.tracks[track].fields[field].display_name}}
+          </v-flex>
+          <v-flex class="field-label" xs12 md3 v-else>
+            {{field}}
+          </v-flex>
+          <v-flex xs12 md9 v-if="accountRequestData.tracks[track].fields[field].display_component">
+            <component v-if="field == 'harvard_key'"
+              :is="accountRequestData.tracks[track].fields[field].display_component"
+              :data="accountRequestData[field]">
+            </component>
+            <component v-else
+              :is="accountRequestData.tracks[track].fields[field].display_component"
+              :data="accountRequestData.person[field]">
+            </component>
+          </v-flex>
+          <v-flex xs12 md9 v-else>
+            <span v-if="accountRequestData.person[field]">{{accountRequestData.person[field]}}</span>
+            <span v-else>{{accountRequestData[field]}}</span>
+          </v-flex>
+          <v-flex>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<style scoped>
+  .field-label {
+    font-weight: bold;
+  }
+</style>
---
+++ src/components/request/DisplayAddressList.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+  name: 'DisplayAddressList',
+  props: [
+    'data'
+  ]
+}
+</script>
+<template>
+  <v-layout column>
+    <v-flex>
+      <v-layout row v-for="address in data" :key="address.type">
+        <v-flex>
+          <v-layout column>
+            <v-flex>
+              <v-layout row>
+                <v-flex xs4>
+                  Street address
+                </v-flex>
+                <v-flex>
+                  {{address.street1}}
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex>
+              <v-layout row>
+                <v-flex xs4>
+                  City
+                </v-flex>
+                <v-flex>
+                  {{address.city}}
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex>
+              <v-layout row>
+                <v-flex xs4>
+                  State
+                </v-flex>
+                <v-flex>
+                  {{address.state}}
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex>
+              <v-layout row>
+                <v-flex xs4>
+                  Country
+                </v-flex>
+                <v-flex>
+                  {{address.country}}
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex>
+              <v-layout row>
+                <v-flex xs4>
+                  Postal code
+                </v-flex>
+                <v-flex>
+                  {{address.postal_code}}
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-flex>
+  </v-layout>
+</template>
+<style scoped>
+
+</style>
