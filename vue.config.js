543be1260382a46b39b3c8eaf1de559e98209b9a
Andy Bryant <a.mcdermid.bryant@gmail.com>
2021-01-06 10:31:39 -0500

    updated externals

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,20 @@
+function getProdExternals() {
+  return {
+    axios: 'axios',
+    lodash: 'lodash',
+    vue: 'Vue',
+    vuetify: 'vuetify',
+    moment: 'moment',
+    vuex: 'vuex',
+    'core-js': 'core-js'
+  };
+}
+
 module.exports = {
   configureWebpack: {
     resolve: {
       extensions: ['*', '.js', '.vue', '.json']
-    }
+    },
+    externals: process.env.NODE_ENV === 'production' ? getProdExternals() : {}
   }
 }
