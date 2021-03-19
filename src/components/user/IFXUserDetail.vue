28b62a8609a46bfc9054e0e11416b7d0fa515e22
DDA <github@dantonio.net>
2021-03-19 10:15:27 -0400

    Add spacer after login icon in user detail

--- src/components/user/IFXUserDetail.vue
+++ src/components/user/IFXUserDetail.vue
@@ -35,7 +35,7 @@ export default {
     <IFXPageHeader>
       <template #title>{{detailTitle}}: {{item.username || id}}</template>
       <template #actions>
-        <IFXLoginIcon disabled v-if='item.isActive !== undefined' :isActive='item.isActive'/>
+        <IFXLoginIcon disabled v-if='item.isActive !== undefined' :isActive='item.isActive' class="mr-1"/>
         <IFXButton btnType="edit" @action="navigateToItemEdit(id)"/>
       </template>
       <template #content>
