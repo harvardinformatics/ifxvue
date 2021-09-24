e6d667a084f55349244cdae3da23fbe7ef46d71e
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2021-09-24 16:45:43 -0400

    Some layout changes to Product pages Make buttons xSmall Ensure that 'next' query parameter is honored for forms Fixed header widths on ProductList

--- src/components/product/IFXProductList.vue
+++ src/components/product/IFXProductList.vue
@@ -16,14 +16,14 @@ export default {
   computed: {
     headers() {
       const headers = [
-        { text: 'ID', value: 'id', sortable: true, slot: true, click: true },
-        { text: 'Product Number', value: 'productNumber', sortable: true, slot: true },
+        { text: 'ID', value: 'id', sortable: true, slot: true, click: true, width: '60px' },
+        { text: 'Product Number', value: 'productNumber', sortable: true, slot: true, width: '150px' },
         { text: 'Name', value: 'name', sortable: true },
-        { text: 'Description', value: 'description', sortable: true },
+        { text: 'Description', value: 'description', sortable: true, width: '150px' },
         { text: 'Facility', value: 'facility', sortable: true, slot: true },
         { text: 'Rates', value: 'rates', sortable: false, namedSlot: true },
-        { text: 'Reporting Group', value: 'reportingGroup', sortable: false, slot: true },
-        { text: '', value: 'rowActionEdit', slot: true },
+        { text: 'Reporting Group', value: 'reportingGroup', sortable: true, slot: true },
+        { text: '', value: 'rowActionEdit', slot: true, sortable: false },
       ]
       return headers.filter((h) => !h.hide || !this.$vuetify.breakpoint[h.hide])
     },
@@ -48,7 +48,7 @@ export default {
           @get-set-items="getSetItems"
           :selectedItems.sync="selected"
         />
-        <IFXButton btnType="add" @action="navigateToItemCreate" />
+        <IFXButton btnType="add" small @action="navigateToItemCreate" />
       </template>
     </IFXPageHeader>
     <IFXItemDataTable :items="filteredItems" :headers="headers" :selected.sync="selected" :itemType="itemType">
