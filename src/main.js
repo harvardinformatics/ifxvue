3fbb0c4a8bd8e67e71d45a01e4bc2e23cfc7d4d2
DDA <github@dantonio.net>
2021-10-06 11:44:07 -0400

    Load local vuetify and set up required info

--- src/main.js
+++ src/main.js
@@ -1,12 +1,36 @@
-import Vue from "vue";
-import App from "./App";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue'
+import APIService from './api/IFXAPI'
+import App from './App'
+import vuetify from './plugins/vuetify-local'
+
 // import 'vuetify/dist/vuetify.min.css'
+// This file contains all application-specific to be used in ifxvue
+
+const appName = 'ifxvue'
+const appNameFormatted = 'IFXVue'
+const urls = []
+
+const vars = {
+  appName,
+  appNameFormatted,
+  appKey: `ifx_${appName}`,
+}
+
+// Initialize with empty user template before authentication occurs
+const APIStore = {
+  urls,
+  vars,
+  ui: {},
+}
+
+const api = new APIService(APIStore)
+Vue.prototype.$api = Vue.observable(api)
+api.auth.initAuthUser()
 
 /* eslint-disable no-new */
 new Vue({
   vuetify,
-  el: "#app",
+  el: '#app',
   components: { App },
-  render: h => h(App),
-});
+  render: (h) => h(App),
+})
