750f54624ab3156a1b37dbefc98cbf73a25fc57b
DDA <github@dantonio.net>
2021-10-06 11:43:42 -0400

    Load icons and set theme in a local vuetify file
    - This is only used by the main.js so shouldn’t be in the library

---
+++ src/plugins/vuetify-local.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-unresolved, import/no-extraneous-dependencies */
+import Vue from 'vue'
+import Vuetify, { VTextField } from 'vuetify'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.use(Vuetify)
+
+// With tree-shaking enabled, webpack won't recognize v-text-field within the currency field
+// Register explicitly
+Vue.component('v-text-field', VTextField)
+
+export default new Vuetify({
+  dark: false,
+  icons: {
+    iconfont: 'mdi',
+  },
+  themes: {
+    light: {
+      primary: '#C62828',
+      secondary: '#90A4AE',
+      accent: '#5C6BC0',
+      error: '#db564c',
+      warning: '#fcf3a1',
+      info: '#2196f3',
+      success: '#4caf50',
+    },
+    dark: {
+      primary: '#C62828',
+      secondary: '#90A4AE',
+      accent: '#5C6BC0',
+      error: '#db564c',
+      warning: '#fcf3a1',
+      info: '#2196f3',
+      success: '#4caf50',
+    },
+  },
+})
