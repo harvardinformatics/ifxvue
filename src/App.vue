fcdb51ba83de2f61616b6cb797d4a9a25e578c95
DDA <github@dantonio.net>
2021-10-06 11:44:22 -0400

    Test harness that loads a button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
-<template>
-  <h1>App</h1>
-</template>
-
 <script>
+import IFXButton from '@/components/IFXButton'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    IFXButton,
+  },
+  mounted() {},
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  watch: {},
 }
 </script>
+<template>
+  <v-app>
+    <v-card>
+      <v-card-title>
+        <v-row>
+          <v-col>
+            Local App
+          </v-col>
+        </v-row>
+      </v-card-title>
+      <v-container>
+        <v-row>
+          <v-col>
+            <IFXButton btnType="submit" btnText="Click me!" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-app>
+</template>
+<style scoped lang="scss"></style>
