f992ad8af1c4e6d0a33ee8a64724c480ae2bc12a
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2021-12-05 15:40:56 -0500

    clean up accidental replace

--- src/components/user/IFXUser.js
+++ src/components/user/IFXUser.js
@@ -2,102 +2,102 @@ import IFXItemBase from '@/components/item/IFXItemBase'
 
 export class UserContact {
   constructor(data) {
-    this.data.account = data
+    this.data = data
     this.role = data.role || 'Additional Contact'
   }
 
   get contact() {
-    return this.data.account.contact
+    return this.data.contact
   }
 
   set contact(contact) {
-    this.data.account.contact = contact
+    this.data.contact = contact
   }
 
   get role() {
-    return this.data.account.role
+    return this.data.role
   }
 
   set role(role) {
-    this.data.account.role = role
+    this.data.role = role
   }
 
   get id() {
-    return this.data.account.id
+    return this.data.id
   }
 
   set id(id) {
-    this.data.account.id = id
+    this.data.id = id
   }
 
   get name() {
-    return this.data.account.contact.name
+    return this.data.contact.name
   }
 
   set name(name) {
-    this.data.account.contact.name = name
+    this.data.contact.name = name
   }
 
   get type() {
-    return this.data.account.contact.type
+    return this.data.contact.type
   }
 
   set type(type) {
-    this.data.account.contact.type = type
+    this.data.contact.type = type
   }
 
   get detail() {
-    return this.data.account.contact.detail
+    return this.data.contact.detail
   }
 
   set detail(detail) {
-    this.data.account.contact.detail = detail
+    this.data.contact.detail = detail
   }
 
   get phone() {
-    return this.data.account.contact.phone
+    return this.data.contact.phone
   }
 
   set phone(phone) {
-    this.data.account.contact.phone = phone
+    this.data.contact.phone = phone
   }
 
   get address() {
-    return this.data.account.contact.address
+    return this.data.contact.address
   }
 
   set address(address) {
-    this.data.account.contact.address = address
+    this.data.contact.address = address
   }
 
   get isValid() {
-    return this.data.account.contact.is_valid
+    return this.data.contact.is_valid
   }
 
   set isValid(isValid) {
-    this.data.account.contact.is_valid = isValid
+    this.data.contact.is_valid = isValid
   }
 }
 
-export class UserAccount {
+export class UserAccount extends IFXItemBase {
   get account() {
-    return this.data.account.account
+    return this.data.account
   }
 
   set account(account) {
-    this.data.account.account = account
+    this.data.account = account
   }
 
   get isValid() {
-    return this.data.account.is_valid
+    return this.data.is_valid
   }
 
   set isValid(isValid) {
-    this.data.account.is_valid = isValid
+    this.data.is_valid = isValid
   }
 
   get code() {
-    return this.data.account.account.code
+    return this.data.account.code
   }
 
   set code(val) {
@@ -170,54 +170,54 @@ export class User extends IFXItemBase {
   constructor(data) {
     super(data)
     // Setting primary email so that when updates are done, the correct Person field is updated
-    this.data.account.primary_email = data.email
+    this.data.primary_email = data.email
     // NOTE: values must be set here explicitly for nested forms to be reactive
     this.addresses = data.addresses || []
     this.contacts = data.contacts || []
   }
 
   get ifxid() {
-    return this.data.account.ifxid
+    return this.data.ifxid
   }
 
   get username() {
-    return this.data.account.username
+    return this.data.username
   }
 
   get isAuthenticated() {
-    return this.data.account.username && this.authToken
+    return this.data.username && this.authToken
   }
 
   get dateJoined() {
-    return this.data.account.date_joined
+    return this.data.date_joined
   }
 
   get lastUpdate() {
-    return this.data.account.last_update
+    return this.data.last_update
   }
 
   get firstName() {
-    return this.data.account.first_name
+    return this.data.first_name
   }
 
   set firstName(firstName) {
-    this.data.account.first_name = firstName
+    this.data.first_name = firstName
   }
 
   get lastName() {
-    return this.data.account.last_name
+    return this.data.last_name
   }
 
   set lastName(lastName) {
-    this.data.account.last_name = lastName
+    this.data.last_name = lastName
   }
 
   get fullName() {
-    return this.data.account.full_name
+    return this.data.full_name
   }
 
   set fullName(fullName) {
-    this.data.account.full_name = fullName
+    this.data.full_name = fullName
   }
 
   get name() {
@@ -229,55 +229,55 @@ export class User extends IFXItemBase {
   }
 
   get email() {
-    return this.data.account.primary_email
+    return this.data.primary_email
   }
 
   set email(email) {
-    this.data.account.primary_email = email
+    this.data.primary_email = email
   }
 
   get primaryEmail() {
-    return this.data.account.primary_email
+    return this.data.primary_email
   }
 
   set primaryEmail(primaryEmail) {
-    this.data.account.primary_email = primaryEmail
+    this.data.primary_email = primaryEmail
   }
 
   get primaryAffiliation() {
-    return this.data.account.primary_affiliation
+    return this.data.primary_affiliation
   }
 
   set primaryAffiliation(primaryAffiliation) {
-    this.data.account.primary_affiliation = primaryAffiliation
+    this.data.primary_affiliation = primaryAffiliation
   }
 
   get title() {
-    return this.data.account.title
+    return this.data.title
   }
 
   set title(title) {
-    this.data.account.title = title
+    this.data.title = title
   }
 
   get groups() {
-    return this.data.account.groups
+    return this.data.groups
   }
 
   set groups(groups) {
-    this.data.account.groups = groups
+    this.data.groups = groups
   }
 
   get isActive() {
-    return this.data.account.is_active
+    return this.data.is_active
   }
 
   set isActive(isActive) {
-    this.data.account.is_active = isActive
+    this.data.is_active = isActive
   }
 
   get isAdmin() {
-    return this.data.account.is_staff
+    return this.data.is_staff
   }
 
   set isAdmin(isAdmin) {
@@ -285,75 +285,75 @@ export class User extends IFXItemBase {
   }
 
   get isDjangoStaff() {
-    return this.data.account.is_staff
+    return this.data.is_staff
   }
 
   set isDjangoStaff(isDjangoStaff) {
-    this.data.account.is_staff = isDjangoStaff
+    this.data.is_staff = isDjangoStaff
   }
 
   get changeComment() {
-    return this.data.account.change_comment
+    return this.data.change_comment
   }
 
   set changeComment(changeComment) {
-    this.data.account.change_comment = changeComment
+    this.data.change_comment = changeComment
   }
 
   get isEnabled() {
-    return this.data.account.is_enabled
+    return this.data.is_enabled
   }
 
   set isEnabled(isEnabled) {
-    this.data.account.is_enabled = isEnabled
+    this.data.is_enabled = isEnabled
   }
 
   get addresses() {
-    return this.data.account.addresses
+    return this.data.addresses
   }
 
   set addresses(addresses) {
-    this.data.account.addresses = addresses
+    this.data.addresses = addresses
   }
 
   get affiliations() {
-    return this.data.account.affiliations
+    return this.data.affiliations
   }
 
   set affiliations(affiliations) {
-    this.data.account.affiliations = affiliations
+    this.data.affiliations = affiliations
   }
 
   get accounts() {
-    return this.data.account.accounts
+    return this.data.accounts
   }
 
   set accounts(accounts) {
-    this.data.account.accounts = accounts
+    this.data.accounts = accounts
   }
 
   get productAccounts() {
-    return this.data.account.product_accounts
+    return this.data.product_accounts
   }
 
   set productAccounts(productAccounts) {
-    this.data.account.product_accounts = productAccounts
+    this.data.product_accounts = productAccounts
   }
 
   get logins() {
-    return this.data.account.logins
+    return this.data.logins
   }
 
   set logins(logins) {
-    this.data.account.logins = logins
+    this.data.logins = logins
   }
 
   get contacts() {
-    return this.data.account.contacts
+    return this.data.contacts
   }
 
   set contacts(contacts) {
-    this.data.account.contacts = contacts
+    this.data.contacts = contacts
   }
 
   hasGroup(group) {
@@ -362,7 +362,7 @@ export class User extends IFXItemBase {
 
   addToGroup(group) {
     if (!(group in this.groups)) {
-      this.data.account.groups.push(group)
+      this.data.groups.push(group)
     }
   }
 
