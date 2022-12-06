73720d9806e38cc173f43202bcfc370b80773436
DDA <github@dantonio.net>
2022-12-06 17:08:51 -0500

    Component to select an org and role for affiliations

---
+++ src/components/affiliation/IFXSelectAffiliation.vue
@@ -0,0 +1,88 @@
+<script>
+import IFXItemSelectableMixin from '@/components/item/IFXItemSelectableMixin'
+
+export default {
+  name: 'IFXSelectAffiliation',
+  mixins: [IFXItemSelectableMixin],
+  data() {
+    return {
+      contactType: null,
+      allRoles: [
+        {
+          value: 'pi',
+          text: 'PI',
+        },
+        {
+          value: 'member',
+          text: 'Member',
+        },
+        {
+          value: 'lab_manager',
+          text: 'Lab Manager',
+        },
+        {
+          value: 'approver',
+          text: 'Approver',
+        },
+      ],
+      search: '',
+      isValid: false,
+    }
+  },
+  mounted() {},
+  computed: {
+    isContactSelected() {
+      return this.itemLocal.contact?.detail
+    },
+    appropriateRoles() {
+      // We assume that the type and the role name both contain the same case-senstive value
+      return this.allRoles.filter(
+        (role) => this.$api.auth.can('edit-affiliations', this.$api.authUser) || role.value === 'member'
+      )
+    },
+  },
+  methods: {},
+  watch: {
+    isValid(valid) {
+      this.$emit('update:valid', valid)
+    },
+  },
+}
+</script>
+<template>
+  <v-container fluid v-if="!isLoading">
+    <v-form ref="form" v-model="isValid">
+      <v-row>
+        <v-col>
+          <v-autocomplete
+            v-model="itemLocal.role"
+            :items="appropriateRoles"
+            :error-messages="errors['role']"
+            :rules="formRules.generic"
+            label="Role"
+            required
+            data-cy="select-role"
+          ></v-autocomplete>
+        </v-col>
+        <v-col>
+          <v-autocomplete
+            v-model="itemLocal.organization"
+            label="Search for an organization"
+            :items="allItems"
+            auto-select-first
+            clearable
+            clear-icon="mdi-close-circle"
+            hide-selected
+            :search-input.sync="search"
+            @change="search = ''"
+            data-cy="select-organization"
+            required
+            :rules="formRules.generic"
+            :error-messages="errors['organization']"
+          >
+          </v-autocomplete>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
