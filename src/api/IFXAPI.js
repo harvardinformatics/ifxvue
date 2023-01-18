bccc4499598c09151f660cec817da603ff497a78
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2023-01-18 14:35:13 -0500

    add Helium to decimal facility list

--- src/api/IFXAPI.js
+++ src/api/IFXAPI.js
@@ -786,7 +786,7 @@ export default class IFXAPIService {
     const baseUrl = this.urls.FACILITIES
     const api = this.genericAPI(baseUrl, Facility)
     api.isDecimalFacility = (facility_name) => {
-      const result = ['Research Computing Storage', 'Center for Brain Science Neuroimaging'].includes(facility_name)
+      const result = ['Research Computing Storage', 'Center for Brain Science Neuroimaging', 'Harvard University Helium'].includes(facility_name)
       return result
     }
     api.isFacilityWithDates = (facility_name) => {
