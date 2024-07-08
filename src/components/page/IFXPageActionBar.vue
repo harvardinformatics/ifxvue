d87ac9f79e84abc69638aa5c57b99d11055ad38f
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2024-07-08 12:26:59 -0400

    just 'updating'

--- src/components/page/IFXPageActionBar.vue
+++ src/components/page/IFXPageActionBar.vue
@@ -49,7 +49,7 @@ export default {
     <v-overlay :value="submitting">
       <span class="d-flex align-center elevation-2 pa-4 rounded-lg grey darken-1">
         <v-progress-circular indeterminate size="64"></v-progress-circular>
-        <em class="ml-4 text-body-1">Our network minions are carrying data to the server...</em>
+        <em class="ml-4 text-body-1">Updating...</em>
       </span>
     </v-overlay>
   </v-row>
