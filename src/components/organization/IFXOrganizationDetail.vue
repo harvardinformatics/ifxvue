0c9617b3d20801ca09dc3962e99a95859ff8bbd8
DDA <github@dantonio.net>
2024-12-18 15:54:36 -0500

    Add dialog and route guard to offer to save changes when leaving the page
    - Also adds warning alert box when where are unsaved changes

--- src/components/organization/IFXOrganizationDetail.vue
+++ src/components/organization/IFXOrganizationDetail.vue
@@ -42,6 +42,9 @@ export default {
         { name: 'Facility Invoice', editable: true },
         { name: 'Facility Invoice CC', editable: true },
       ],
+      leaveDialogOpen: false,
+      leaveDialogPromiseResolve: null,
+      leaveDialogPromiseReject: null,
     }
   },
   mounted() {},
@@ -148,7 +151,39 @@ export default {
           })
         })
       }
-      this.submitUpdate()
+      try {
+        this.leaveDialogOpen = false
+        this.submitUpdate()
+        if (this.leaveDialogPromiseResolve) {
+          this.leaveDialogPromiseResolve()
+        }
+      } catch (error) {
+        if (this.leaveDialogPromiseReject) {
+          this.leaveDialogPromiseReject()
+        }
+      } finally {
+        this.leaveDialogPromisResolve = null
+        this.leaveDialogPromisReject = null
+      }
+    },
+    confirmLeave() {
+      return new Promise((resolve, reject) => {
+        this.leaveDialogOpen = true
+        this.leaveDialogPromisResolve = resolve
+        this.leaveDialogPromisReject = reject
+      })
+    },
+    cancelLeave() {
+      this.leaveDialogOpen = false
+      this.leaveDialogPromisReject()
+      this.leaveDialogPromisResolve = null
+      this.leaveDialogPromisReject = null
+    },
+    leavePage() {
+      this.leaveDialogOpen = false
+      this.leaveDialogPromisResolve()
+      this.leaveDialogPromisResolve = null
+      this.leaveDialogPromisReject = null
     },
   },
   computed: {
@@ -174,6 +209,20 @@ export default {
       return this.item?.users?.filter((user) => this.showInactive || user.active)
     },
   },
+  beforeRouteLeave(to, from, next) {
+    // eslint-disable-line
+    if (this.hasItemChanged()) {
+      this.confirmLeave()
+        .then(() => {
+          next()
+        })
+        .catch(() => {
+          next(false)
+        })
+    } else {
+      next()
+    }
+  },
 }
 </script>
 
@@ -190,6 +239,17 @@ export default {
         </div>
       </template>
     </IFXPageHeader>
+    <v-row dense v-if="isSubmittable">
+      <v-col>
+        <v-alert elevation="2" dense border="left" light color="warning" icon="mdi-alert-circle-outline">
+          <v-row dense>
+            <v-col>
+              <h3 class="font-weight-medium">You have unsaved changes!</h3>
+            </v-col>
+          </v-row>
+        </v-alert>
+      </v-col>
+    </v-row>
     <v-row dense class="ml-2">
       <v-col>
         <v-row>
@@ -378,6 +438,30 @@ export default {
           <v-btn small text class="mr-2" :disabled="!addContactFormIsValid" color="primary" @click="addContact()">
             Add
           </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="leaveDialogOpen" v-if="leaveDialogOpen" max-width="600px" @keydown.esc="cancel">
+      <v-card>
+        <v-toolbar dark color="warning" flat>
+          <v-toolbar-title class="black--text">Save changes?</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon small @click="cancelLeave" data-cy="leave-dialog-close" v-on="on" v-bind="attrs">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </template>
+            <span>Cancel</span>
+          </v-tooltip>
+        </v-toolbar>
+        <v-card-text class="pa-4 black--text text-body-1">
+          You have unsaved changes. Do you want to save them before leaving the page?
+        </v-card-text>
+        <v-card-actions class="pt-3">
+          <v-spacer></v-spacer>
+          <v-btn color="secondary" text class="" @click.native="leavePage">Leave</v-btn>
+          <v-btn color="primary" text class="" @click.native="updateUsersAndSubmit">Save</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
