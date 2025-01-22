c85fd654280e9a81c09adb4ebc4499f052032aa3
DDA <github@dantonio.net>
2025-01-22 14:27:02 -0500

    Fix the check on end month being 0
    - We want to end on the previous month so we use `getMonth()`, which is zero-based (i.e. one less than we normally refer to months). But we need to check if we’re at month 0 (Jan) and back it off to Dec of the previous year.
    - Fix the check for above.

--- src/components/billingSummary/IFXLabBillingSummaryList.vue
+++ src/components/billingSummary/IFXLabBillingSummaryList.vue
@@ -38,10 +38,10 @@ export default {
     }
   },
   mounted() {
-    if (!this.startMonth) {
+    if (!this.endMonth) {
       // If we're at the start of the year, we need to go back to the previous year
       // Since getMonth() is zero-based, we're using that to go to the previous month
-      this.startMonth = 12
+      this.endMonth = 12
       this.endYear--
     }
     this.startYear = this.endYear
