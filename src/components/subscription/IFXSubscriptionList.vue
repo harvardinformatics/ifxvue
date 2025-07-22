1e83c5587ff15de69d0b94f442943922d5534074
DDA <github@dantonio.net>
2025-07-22 11:46:05 -0400

    Add `subscription` pieces from `fiine`

---
+++ src/components/subscription/IFXSubscriptionList.vue
@@ -0,0 +1,133 @@
+<script>
+import IFXItemDataTable from '@/components/item/IFXItemDataTable'
+import IFXSearchField from '@/components/IFXSearchField'
+import IFXItemListMixin from '@/components/item/IFXItemListMixin'
+import SubscriptionMixin from '@/components/subscription/SubscriptionMixin'
+
+export default {
+  name: 'IFXSubscriptionList',
+  mixins: [IFXItemListMixin, SubscriptionMixin],
+  components: {
+    IFXSearchField,
+    IFXItemDataTable,
+  },
+  props: {},
+  data() {
+    return {
+      key: 0,
+      currentUserRecord: {},
+    }
+  },
+  methods: {
+    async getSetItems() {
+      this.isLoading = true
+      return this.apiRef
+        .getList()
+        .then((res) => {
+          const items = res.map((item) => {
+            item.subscribed = !!item.subscriptionId
+            return item
+          })
+          this.items = items
+        })
+        .catch((error) => {
+          this.showMessage(error)
+        })
+        .finally(() => (this.isLoading = false))
+    },
+    subscribeToChannel(item) {
+      this.$api.subscription
+        .subscribeToChannel(this.currentUserRecord.id, item.channelId)
+        .then(() => {
+          this.showMessage(`Subscribed to channel ${item.channelTitle}`)
+        })
+        .catch((error) => {
+          this.showMessage(error)
+        })
+        .finally(() => {
+          this.reloadSubscriptions()
+        })
+    },
+    unsubscribeFromChannel(item) {
+      this.$api.subscription
+        .unsubscribeFromChannel(item.subscriptionId)
+        .then(() => {
+          this.showMessage(`Unsubscribed from channel ${item.channelTitle}`)
+        })
+        .catch((error) => {
+          this.showMessage(error)
+        })
+        .finally(() => {
+          this.reloadSubscriptions()
+        })
+    },
+    async reloadSubscriptions() {
+      await this.getSetItems()
+      this.key += 1
+    },
+    toggleSubscription(item) {
+      if (item.subscriptionId) {
+        this.unsubscribeFromChannel(item)
+      } else {
+        this.subscribeToChannel(item)
+      }
+    },
+  },
+  computed: {
+    headers() {
+      const headers = [
+        { text: 'Channel ID', value: 'channelId', sortable: true },
+        { text: 'Title', value: 'channelTitle', sortable: true },
+        { text: 'Organization', value: 'organizationName', sortable: true },
+        { text: 'Subscribed', value: 'actions', sortable: false, namedSlot: true },
+      ]
+      return headers.filter((h) => !h.hide || !this.$vuetify.breakpoint[h.hide])
+    },
+  },
+  watch: {},
+  async mounted() {
+    this.currentUserRecord = await this.$api.auth.getCurrentUserRecord()
+  },
+}
+</script>
+
+<template>
+  <v-container>
+    <IFXPageHeader>
+      <template #title>Channel Subscriptions</template>
+      <template #actions>
+        <IFXSearchField :search.sync="search" />
+      </template>
+    </IFXPageHeader>
+    <v-container>
+      <IFXItemDataTable
+        :items="filteredItems"
+        :headers="headers"
+        :selected.sync="selected"
+        :itemType="itemType"
+        :show-select="false"
+        :loading="isLoading"
+        sortBy="channelTitle"
+        :sortDesc="false"
+      >
+        <template v-slot:actions="{ item }">
+          <v-switch
+            v-model="item.subscribed"
+            data-cy="toggle-subscription"
+            @change="toggleSubscription(item)"
+          ></v-switch>
+        </template>
+      </IFXItemDataTable>
+    </v-container>
+  </v-container>
+</template>
+<style scoped>
+.ellipsis {
+  max-width: 200px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  display: inline-block;
+  max-height: 100px;
+}
+</style>
