3769429045f96538705f6c7450a537b4bf62ad91
Antonio Rodriguez <antoniorodriguez@theagilemonkeys.com>
2026-02-03 18:14:04 +0100

    Fix create phone contact

--- src/components/contact/IFXContactCreateEdit.vue
+++ src/components/contact/IFXContactCreateEdit.vue
@@ -25,7 +25,8 @@ export default {
     return {
       allUsers: [],
       localContactType: this.contactType,
-      item: null
+      item: null,
+      isLoading: false,
     }
   },
   methods: {
@@ -33,7 +34,6 @@ export default {
     async init() {
       this.item = await this.getItem()
       this.cachedItem = JSON.parse(JSON.stringify(this.item))
-      // TODO: only get chunks
       this.allUsers = await this.$api.user.getList().catch((err) => this.showMessage(err))
     },
     updateContactUsers(users) {
@@ -46,16 +46,20 @@ export default {
   computed: {
     title() {
       const createOrEdit = this.id ? 'Edit' : 'Create'
-      const type = this.localContactType ? `${this.localContactType} Contact` : ' Contact'
+      const type = this.localContactType ? `${this.localContactType} Contact` : 'Contact'
       const id = this.item && this.item.name ? `for ${this.item.name}` : this.id || ''
       return `${createOrEdit} ${type} ${id}`
     },
+    description() {
+      return ''
+    },
     isEditing() {
       return this.id !== null && this.id !== ''
-    }
+    },
   },
   mounted() {
     if (this.id) {
+      this.isLoading = true
       this.$api.contact.getByID(this.id)
         .then((contact) => {
           this.item = contact
@@ -74,8 +78,11 @@ export default {
         .catch((error) => {
           this.showMessage(error)
         })
+        .finally(() => {
+          this.isLoading = false
+        })
     }
-  }
+  },
 }
 </script>
 
@@ -83,46 +90,34 @@ export default {
   <v-container v-if="!isLoading">
     <IFXPageHeader>
       <template #title>{{ title }}</template>
-      <template #content>{{ description }}</template>
+      <template #content v-if="description">{{ description }}</template>
     </IFXPageHeader>
     <v-container>
       <v-row>
-        <v-col class="d-flex justify-start">
+        <v-col class="d-flex justify-start align-center ga-4">
           <span>Contact type</span>
           <v-radio-group
             v-model="localContactType"
             inline
+            hide-details
           >
-            <v-radio
-              label="Email"
-              value="Email"
-            >
-            </v-radio>
-            <v-radio
-              label="Phone"
-              value="Phone"
-            >
-            </v-radio>
-            <v-radio
-              label="Full"
-              value="Full"
-            >
-            </v-radio>
+            <v-radio label="Email" value="Email" />
+            <v-radio label="Phone" value="Phone" />
+            <v-radio label="Full" value="Full" />
           </v-radio-group>
         </v-col>
       </v-row>
       <v-row>
         <v-col v-if="localContactType === 'Email'">
-          <IFXEmailContactCreateEdit :isEditing="isEditing" :id="id"/>
+          <IFXEmailContactCreateEdit :isEditing="isEditing" :id="id" />
         </v-col>
         <v-col v-else-if="localContactType === 'Phone'">
-          <IFXPhoneContactCreateEdit :isEditing="isEditing" :id="id"/>
+          <IFXPhoneContactCreateEdit :isEditing="isEditing" :id="id" />
         </v-col>
         <v-col v-else-if="localContactType === 'Full'">
-          <IFXFullContactCreateEdit :isEditing="isEditing" :id="id"/>
+          <IFXFullContactCreateEdit :isEditing="isEditing" :id="id" />
         </v-col>
-        <v-col v-else>
-        </v-col>
+        <v-col v-else />
       </v-row>
     </v-container>
   </v-container>
