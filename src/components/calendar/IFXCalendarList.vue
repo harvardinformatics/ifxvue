706daa4ddbe507329a15b851a0d41117a5a3517d
DDA <github@dantonio.net>
2026-02-27 10:25:43 -0500

    `item.raw` is now just `item`…again

--- src/components/calendar/IFXCalendarList.vue
+++ src/components/calendar/IFXCalendarList.vue
@@ -996,14 +996,14 @@ export default {
           data-cy="filter-resources"
         >
           <template #selection="{ item }">
-            <v-chip :color="item.raw.color" variant="flat" closable @click:close="removeFromFiltered(item.raw)">
-              {{ item.raw.name }}
+            <v-chip :color="item.color" variant="flat" closable @click:close="removeFromFiltered(item)">
+              {{ item.name }}
             </v-chip>
           </template>
           <template #item="{ item, props }">
             <v-list-item v-bind="props">
               <template #prepend>
-                <div :style="$api.resource.resourceColorBox(item.raw)" class="mr-2">&nbsp;</div>
+                <div :style="$api.resource.resourceColorBox(item)" class="mr-2">&nbsp;</div>
               </template>
             </v-list-item>
           </template>
@@ -1065,7 +1065,7 @@ export default {
               :event-color="getEventColor"
               :event-timed="isEventTimed"
               :type="type"
-              interval-height="60"
+              xxinterval-height="60"
               event-text-color="black"
               @click:more="viewDay"
               @click:date="viewDay"
@@ -1369,18 +1369,18 @@ export default {
                     <template #item="{ item, props }">
                       <v-list-item v-bind="props">
                         <template #prepend>
-                          <v-icon :color="$api.reservation.getUserIconColor(item.raw)">
+                          <v-icon :color="$api.reservation.getUserIconColor(item)">
                             {{ $api.reservation.getUserIcon() }}
                           </v-icon>
                         </template>
                       </v-list-item>
                     </template>
                     <template #selection="{ item }">
-                      <v-chip variant="text" closable @click:close="removeFromSelected(item.raw)">
-                        <v-icon :color="$api.reservation.getUserIconColor(item.raw)" class="mr-2">
+                      <v-chip variant="text" closable @click:close="removeFromSelected(item)">
+                        <v-icon :color="$api.reservation.getUserIconColor(item)" class="mr-2">
                           {{ $api.reservation.getUserIcon() }}
                         </v-icon>
-                        {{ item.raw.fullName }}
+                        {{ item.fullName }}
                       </v-chip>
                     </template>
                   </v-autocomplete>
