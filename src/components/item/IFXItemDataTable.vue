5cd04725c50eab2cc404c78fe5812a8d3c6433e9
DDA <github@dantonio.net>
2026-06-03 11:46:43 -0400

    Allow Vuetify 2 defaults for table headers in `IFXItemDataTable`
    - If `title` or `key` is passed, use that, else use the Vuetify `text` and `value` instead.
    - This will make migrations easier

--- src/components/item/IFXItemDataTable.vue
+++ src/components/item/IFXItemDataTable.vue
@@ -134,13 +134,25 @@ export default {
       return `${this.itemType}TableItemsPerPage`
     },
     permissionCheckedHeaders() {
-      return this.headers.filter((h) => (h.permission !== undefined ? h.permission : true))
+      return this.headers
+        .filter((h) => (h.permission !== undefined ? h.permission : true))
+        .map((header) => ({
+          ...header,
+          title: header.title ?? header.text,
+          key: header.key ?? header.value,
+        }))
     },
     sortByOptions() {
       if (Array.isArray(this.sortBy)) {
         return this.sortBy.map((key, index) => ({
           key,
-          order: Array.isArray(this.sortDesc) ? (this.sortDesc[index] ? 'desc' : 'asc') : (this.sortDesc ? 'desc' : 'asc')
+          order: Array.isArray(this.sortDesc)
+            ? this.sortDesc[index]
+              ? 'desc'
+              : 'asc'
+            : this.sortDesc
+            ? 'desc'
+            : 'asc',
         }))
       }
       return [{ key: this.sortBy, order: this.sortDesc ? 'desc' : 'asc' }]
@@ -179,7 +191,9 @@ export default {
     </template>
 
     <template #header.rowActionEdit="{ header }">
-      <span class="d-sr-only" :key="header?.key || 'rowActionEdit'">Buttons to go to the Edit page for the item in each row</span>
+      <span class="d-sr-only" :key="header?.key || 'rowActionEdit'">
+        Buttons to go to the Edit page for the item in each row
+      </span>
     </template>
 
     <template #header.rowActionCopy="{ header }">
