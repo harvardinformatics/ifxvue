d8a35ab50c6687828a490a444a1823935e1183af
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2026-06-20 10:58:58 -0400

    Setup IFXDisplayBillingContact for showing billing contact data; used in BauerCAT Updated entered affiliation to show the primary affiliation if it's set, rather than the entered data

---
+++ src/components/request/IFXDisplayBillingContact.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: 'IFXDisplayBillingContact',
+  props: ['data'],
+}
+</script>
+<template>
+  <v-row v-if="data" dense>
+    <v-col>
+      <v-row dense>
+        <v-col>
+          {{ data.billing_contact.name}}
+        </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col>
+          {{ data.billing_contact.email}}
+        </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col class="text-pre-wrap">
+          {{ data.billing_contact.address}}
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+<style scoped></style>
